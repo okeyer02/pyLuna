5521946208192be138749dd3760a3a827279b5ab
okeyer02 <okeyerxeon@gmail.com>
2023-11-02 23:07:38 +0600

    Update skriptLuna.py

--- skriptLuna.py
+++ skriptLuna.py
@@ -1,5 +1,15 @@
-numericCode="1234 5678 1234 5678"
+numericCode="5062 8212 3456 7892" #banc card code
+
 numericCode=numericCode.replace(" ","")
 if(len(numericCode)!=16):
     print("notCorrected")
-print(numericCode)
+sum=0
+for i in range(0,16,2):
+    sum+=(int(numericCode[i])*2-1)%9+1
+for i in range(1,16,2):
+    sum+=(int(numericCode[i]))
+
+if(sum%10==0):
+    print("true")   #if this card can exist
+else:
+    print("false")  #if this card can't exist
